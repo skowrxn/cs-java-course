d337c8482ecc694da319b18f3378a81c0bb832da
Bartłomiej <bartomiejskowron@icloud.com>
2025-10-01 18:21:51 +0200

    initial commit (lab1)

---
+++ src/main/java/pl/skowrxn/lab1/Main.java
@@ -0,0 +1,7 @@
+package pl.skowrxn.laby;
+
+public class Main {
+
+    public static void main(String[] args){
+    }
+}
---
+++ src/main/java/pl/skowrxn/lab1/Problem610A.java
@@ -0,0 +1,22 @@
+package pl.skowrxn.laby;
+
+import java.util.Scanner;
+
+public class Problem610A {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int result = 0;
+        int i = 1;
+        int b = (n-2*i)/2;
+        while (i < b) {
+            result += 1;
+            i++;
+            b = (n-(2*i))/2;
+        }
+        System.out.println(result);
+    }
+
+}
